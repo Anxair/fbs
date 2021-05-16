8d9f03754f67a44e620c79e95fc2f0eb9ab494bd
Nastya Khripovich <nastyakhripovich@coherentsolutions@com>
2021-05-17 00:59:29 +0300

    directive resize

--- src/app/directives/all-size-win.directive.ts
+++ src/app/directives/all-size-win.directive.ts
@@ -1,16 +1,34 @@
 import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {fromEvent, Observable, Subscription} from 'rxjs';
 
 @Directive({
   selector: '[allSize]'
 })
 export class AllSizeWinDirective implements OnInit {
 
+  resizeObservable$: Observable<Event>;
+  resizeSubscription$: Subscription;
+
   constructor(private element: ElementRef, private renderer: Renderer2) {
   }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.element.nativeElement, 'height', '100%');
+    this.resizeObservable$ = fromEvent(window, 'resize');
     this.renderer.setStyle(this.element.nativeElement, 'overflow', 'auto');
+    this.resizeSubscription$ = this.resizeObservable$.subscribe(this.changeSize.bind(this));
+    this.changeSize();
   }
 
+  private changeSize(): void {
+    const top = this.element.nativeElement.getBoundingClientRect().top;
+    const left = this.element.nativeElement.getBoundingClientRect().left;
+    const sizeHeight = (window.visualViewport.height);
+    const sizeWidth = (window.visualViewport.width);
+    this.renderer.setStyle(this.element.nativeElement, 'height', (sizeHeight - top) + 'px');
+    this.renderer.setStyle(this.element.nativeElement, 'width', (sizeWidth - left) + 'px');
+  }
+
+  ngOnDestroy(): void {
+    this.resizeSubscription$.unsubscribe();
+  }
 }
